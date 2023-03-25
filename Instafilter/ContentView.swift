e0fd33a6ba14aa0e338655c80e3a780f8b695e8a
Yashraj Jadhav <yashrajjadhav49@gmail.com>
2023-03-25 22:00:20 +0530

    Update ContentView.swift

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -4,45 +4,84 @@
 //
 //  Created by Yashraj jadhav on 13/03/23.
 //
-import PhotosUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
-struct ImagePicker: UIViewControllerRepresentable {
-    @Binding var image: UIImage?
-
-    func makeUIViewController(context: Context) -> PHPickerViewController {
-        var config = PHPickerConfiguration()
-        config.filter = .images
-        let picker = PHPickerViewController(configuration: config)
-        picker.delegate = context.coordinator
-        return picker
-    }
-
-    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
-
-    }
-
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
-
-    class Coordinator: NSObject, PHPickerViewControllerDelegate {
-        let parent: ImagePicker
-
-        init(_ parent: ImagePicker) {
-            self.parent = parent
-        }
-
-        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-            picker.dismiss(animated: true)
-
-            guard let provider = results.first?.itemProvider else { return }
-
-            if provider.canLoadObject(ofClass: UIImage.self) {
-                provider.loadObject(ofClass: UIImage.self) { image, _ in
-                    self.parent.image = image as? UIImage
+struct ContentView: View {
+    @State private var filterIntensity = 0.5
+    @State private var image: Image?
+    @State private var showingImagePicker = false
+    @State private var inputImage : UIImage?
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(.secondary)
+                    
+                    Text("Tap to select a picture")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                    
+                    image?
+                        .resizable()
+                        .scaledToFit()
+                }
+                .onTapGesture {
+                    showingImagePicker = true
+                }
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Button("Change Filter") {
+                        // change filter
+                    }
+                    
+                    Spacer()
+                    
+                    Button("Save" , action: save)
                 }
             }
+                
+                
+                .padding([.horizontal , .bottom])
+                .navigationTitle("Instafilter")
+                
+            }
         }
+        
+    func save () {
+        
+    }
+    
+    func applyProcessing() {
+    
+        currentFilter.intensity = Float(filterIntensity)
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
+        
+    }
+}
+        
+        
+    
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
